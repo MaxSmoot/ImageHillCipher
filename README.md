a1531b09a47558e30418e91c3bc25c83bf152b2d
Max Smoot <69818429+MaxSmoot@users.noreply.github.com>
2021-05-06 19:41:01 -0400

    cleaned up code

--- README.md
+++ README.md
@@ -4,31 +4,29 @@
 ## How To Use
 **Images Must be in the .jpg or .tiff format**
 
-**Example**
-
 **Encrypting**
 `python3 main.py -e --image=test_files/test-large.jpg`
 
 **Decrypting**
-`python3 main.py -d --image=encoded.tiff --key=key.tiff`
+`python3 main.py -d --image=encyrpted.tiff --key=key.tiff`
 
-### Encoding
+### Encrypting
 `python3 main.py -e [-c] [--block_size=<int>] --image=<image>`
 This uses the quickest method for generating a random key (uniformly applying a single 3x3 key)
 
-This will output, in the same directory: **encoded.tiff** and **key.tiff**
+This will output, in the same directory: **encrypted.tiff** and **key.tiff**
 
-### Decoding
+### Decrypting
 
-`python3 main.py -d [-c] [--block_size=<int>] --image=<encoded image> --key=<key>`
-This will output, in the same directory: **decoded.jpg**
+`python3 main.py -d [-c] [--block_size=<int>] --image=<encrypted image> --key=<key>`
+This will output, in the same directory: **decrypted.jpg**
 
 
-### Optional Keys (Must match when Enciphering and Deciphering)
+### Optional Keys (Must match when Encrypting and Decrypting)
 
 - `--block_size=<int>` The size of the key and the subsize of the image that will be encrypted in blocks (Must be positive, generally anything larger than 15 will take a lot of time). The image will be padded with black to ensure width and length are evenly divisible by the block_size.
 
-- `-c` Will use a unique key for every subsection of the image instead of uniformly applying a single key. **This will massively increase time to compute**. Outputs **Complex-encoded-tiff** and **Complex-key.tiff**
+- `-c` Will use a unique key for every subsection of the image instead of uniformly applying a single key. **This will massively increase time to compute**. Outputs **Complex-encrypted-tiff** and **Complex-key.tiff**
 
 ## Dependencies
 - python 3
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import numpy as np
-from sympy import Matrix
 import HillCipher as Hill
 import ImageManip as IManip
 import sys
@@ -8,18 +7,18 @@ import getopt
 
 
 def main(argv):
-    encoding = False
-    decoding = False
+    encrypting = False
+    decrypting = False
     complexKey = False
     input_image = None
     key_image = None
     block_size = 3
-    opts, args = getopt.getopt(argv, "edcb", ["key=", "image=", "block_size="])
+    opts, _ = getopt.getopt(argv, "edcb", ["key=", "image=", "block_size="])
     for opt, arg in opts:
         if(opt == '-e'):
-            encoding = True
+            encrypting = True
         elif(opt == "-d"):
-            decoding = True
+            decrypting = True
         elif(opt == "-c"):
             complexKey = True
             print("Using nonuniform encryption, this will take much longer")
@@ -37,13 +36,13 @@ def main(argv):
         if(opt == "--key"):
             key_image = arg
 
-    if(not(encoding != decoding)):
+    if(encrypting == decrypting):
         print("must specify either -e or -d")
         sys.exit()
     input_image = Image.open(input_image)
     image_pixels = np.array(input_image)
-    if(encoding):
-        print("Encrypting... Will output to:", "Complex-encoded.tiff" if complexKey else "encoded.tiff",
+    if(encrypting):
+        print("Encrypting... Will output to:", "Complex-encrypted.tiff" if complexKey else "encrypted.tiff",
               "Complex-key.tiff" if complexKey else "key.tiff")
         padded_image = IManip.padImage(image_pixels, block_size)
         image_pixels = np.array(padded_image)
@@ -51,18 +50,18 @@ def main(argv):
             image_pixels[0]), block_size, complexKey)
         encodedImage = Hill.encodeImage(key, padded_image, block_size)
         encodedImage.save(
-            "Complex-encoded.tiff" if complexKey else "encoded.tiff", None)
+            "Complex-encrypted.tiff" if complexKey else "encrypted.tiff", None)
         key.save("Complex-key.tiff" if complexKey else "key.tiff", None)
         print("Success!")
 
-    elif(decoding):
-        print("Decoding... Will output to: Decoded.jpg")
+    elif(decrypting):
+        print("Decrypting... Will output to: Decrypted.jpg")
         print("All optional flags must match the original encryption")
         key_image = Image.open(key_image)
         decodedImage = Hill.decodeImage(
             key_image, input_image, block_size, complexKey)
         decodedImageSave = Image.fromarray(np.uint8(decodedImage))
-        decodedImageSave.save("decoded.jpg")
+        decodedImageSave.save("decrypted.jpg")
         print("Success!")
 
 
